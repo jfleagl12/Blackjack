3039c5c7222d4618073963fdbaf8cb55d0a1a1fd
Jason Fleagle <jasonfleagle@hotmail.com>
2020-09-02 20:33:11 -0400

    first commit

---
+++ blackjack.py
@@ -0,0 +1,12 @@
+
+# Dealer Cards
+# Player Cards
+# display the Cards
+# Sum of the dealer cards 
+# Sum of the player cardsCompare the sums of the cards between D v P
+# If P card sum is greater than 21 = bust
+# If P card sum is less than 21 = Option Hit or Stay
+# If P option stay compare sum of D v P 
+# If P sum < 21 && > D Sum then P Wins!
+# If P sum < D sum then P loses
+
